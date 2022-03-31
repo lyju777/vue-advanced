fb09a4cc31de66328dfba7981ce1823f7bd207c1
lyju777 <lyju777@hanmail.net>
2022-03-31 23:02:04 +0900

    axios를 이용한 api호출

--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -1,15 +1,30 @@
 <template>
   <div>
-      news
+      <div v-for="(user,i) in users" :key="i">{{user}}</div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
 
+  data(){
+    return{
+      users: []
+    }
+  },
+  created() {
+    var vm = this;
+    axios.get('https://api.hnpwa.com/v0/news/1.json')
+    .then(function(response){
+        console.log(response);
+        vm.users = response.data;
+    })
+    .catch( )
+  }
 }
 </script>
 
 <style>
 
-</style>
+</style> 
